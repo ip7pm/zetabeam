72745950ea864dda568eb6528ec20d27b59e2c65
ip7pm <contact@ip7.pm>
2024-11-15 18:03:09 +0100

    add list() and alive?() to beam::process

--- lib/process.rb
+++ lib/process.rb
@@ -3,37 +3,49 @@ module Beam
   class Process
     @processes = {}
 
-    def self.spawn(klass, method, args)
-      # Generate a PID
-      # TODO: Make sure the pid does not already exists (must be unique)
-      pid = "#PID<0.#{rand(0..500)}.#{rand(0..500)}>"
+    class << self
+      def list()
+        @processes.keys()
+      end
 
-      # Run klass.method in a separate thread
-      ki = klass.new pid
+      def alive?(pid)
+        info = @processes[pid]
+        return false if info.nil?
+        _ki, t = info
+        t.alive?
+      end
 
-      t = Thread.new {
-        ki.send method, *args
-        @processes.delete pid
-      }
+      def spawn(klass, method, args)
+        # Generate a PID
+        # TODO: Make sure the pid does not already exists (must be unique)
+        pid = "#PID<0.#{rand(0..500)}.#{rand(0..500)}>"
 
-      # Maintain the list of running processes
-      @processes.store pid, [ki, t]
-      pid
-    end
+        # Run klass.method in a separate thread
+        ki = klass.new pid
 
-    def self.msg(pid, msg)
-      # Retreive the process
-      info = @processes[pid]
-      if info.nil?
-        # TODO: What to do if process does not exists
-        raise "The process pid: #{pid} does not exists"
-      else
-        # Send message
-        ki, _t = info
-        ki.push_msg_in_mailbox msg
+        t = Thread.new {
+          ki.send method, *args
+          @processes.delete pid
+        }
+
+        # Maintain the list of running processes
+        @processes.store pid, [ki, t]
+        pid
+      end
+
+      def msg(pid, msg)
+        # Retreive the process
+        info = @processes[pid]
+        if info.nil?
+          # TODO: What to do if process does not exists
+          raise "The process pid: #{pid} does not exists"
+        else
+          # Send message
+          ki, _t = info
+          ki.push_msg_in_mailbox msg
+        end
       end
     end
-
   end
 
 end
